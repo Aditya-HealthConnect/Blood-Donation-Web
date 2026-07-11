14f8a9e148056c675169c42a3595681b798ad5c9
BollareddyPranoyRaj1 <bollareddypranoyraj@gmail.com>
2026-07-11 22:08:43 +0530

    Add frontend navigation component

---
+++ bd_frontend/src/components/Navigation.jsx
@@ -0,0 +1,14 @@
+function Navigation() {
+  return (
+    <header className="topbar">
+      <a className="brand" href="/">
+        Blood Donation Web
+      </a>
+      <a className="login-button" href="#login">
+        Login
+      </a>
+    </header>
+  )
+}
+
+export default Navigation
